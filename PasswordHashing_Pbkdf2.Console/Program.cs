cf61ed28472f5b7864f65026f029421f105588e5
stefan @zold <stefan@zold.org>
2017-10-11 23:16:47 +0200

    Console app added for pw in mem dump

---
+++ PasswordHashing_Pbkdf2.Console/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PasswordHashing_Pbkdf2.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Please insert your password for hash:");
+            var inserted = System.Console.ReadLine();
+
+            var encryptor = new Encryptor();
+            var pwHash = encryptor.GetPasswordHash(inserted);
+
+            System.Console.WriteLine($"Hash for password is {pwHash} {Environment.NewLine}");
+
+            System.Console.WriteLine("press enter for exit.");
+            System.Console.ReadLine();
+        }
+    }
+}
